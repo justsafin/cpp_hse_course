f4b1896140cd09ca2636497750259ac370d07048
Fedor Petriaikin <Feda.Petraykin@gmail.com>
2024-02-10 06:07:55 +0300

    Fix tcp load

--- projects/tcp_load/obf_tcp_load_v1.2.py
+++ projects/tcp_load/obf_tcp_load_v1.2.py
@@ -15,7 +15,7 @@ N=int
 J=print
 I=id
 A=None
-import asyncio as L,argparse as a,datetime as G,logging as E,statistics as T,socket as O
+import asyncio as L,argparse as a,datetime as E,logging as F,statistics as T,socket as O
 C=a.ArgumentParser()
 C.add_argument('--host',type=str,required=S,help='Host for loading')
 C.add_argument('--port',type=N,required=S,help='Port on host')
@@ -30,50 +30,51 @@ B=C.parse_args()
 if B.rate is not A and B.parallel is not A:raise Q('Cannot specify --rate and --parallel simultaniously!')
 if B.rate is A and B.parallel is A:raise Q('Please, specify --rate or --parallel!')
 if B.rate is not A:raise Q('--rate is not now supported!')
-E.getLogger().setLevel(getattr(E,B.loglevel))
+F.getLogger().setLevel(getattr(F,B.loglevel))
 def b():
-	def E():
+	def D():
 		with open(B.input_file,'r')as C:
 			for A in C:
 				A=A.strip()
 				if not A:continue
 				yield A
-	C=0;D=0
+	C=0
 	while S:
-		if not B.infinite and C!=0:yield(A,A)
-		for F in E():yield(D,F);D+=1
-		C+=1
+		for E in D():yield(C,E);C+=1
+		if not B.infinite:return A,A
 c=b()
 H=0
 M=0
 D=[]
-F=dict()
+G=dict()
 async def d(reader,writer,q,q_num):B=b'\n';A=writer;A.write(q.encode('utf-8')+B);await A.drain();D=1024;C=await reader.readuntil(B);return C
-async def e():
+async def e(clid):
 	global H,M;C,P=A,A
 	for(J,Q)in c:
 		if Q is A:break
 		if not B.keepalive or C is A:
 			if C is not A:
 				C.close()
-				if I(C)in F:del F[I(C)]
-			try:R=G.datetime.now();N=O.socket(O.AF_INET,O.SOCK_STREAM);N.connect((B.host,B.port));N.setblocking(Y);P,C=await L.open_connection(sock=N);F[I(C)]=N
-			except W as K:E.error(f"{J}: Error open connection: {K}");H+=1;continue
-		try:S=await L.wait_for(d(P,C,Q,J),B.timeout);D.append((G.datetime.now()-R).total_seconds()*1000);E.info(f"{J}: Success in {D[-1]} ms, data = {S}")
-		except TimeoutError as K:E.warning(f"{J}: Timeout: {K}");M+=1
-		except W as K:E.error(f"{J}: Error during i/o: {K}");H+=1
+				if I(C)in G:del G[I(C)]
+			try:R=E.datetime.now();N=O.socket(O.AF_INET,O.SOCK_STREAM);N.connect((B.host,B.port));N.setblocking(Y);P,C=await L.open_connection(sock=N);G[I(C)]=N
+			except W as K:F.error(f"{J}: Error open connection: {K}");H+=1;continue
+		try:
+			if B.keepalive:R=E.datetime.now()
+			S=await L.wait_for(d(P,C,Q,J),B.timeout);D.append((E.datetime.now()-R).total_seconds()*1000);F.info(f"{J}: Success in {D[-1]} ms, data = {S}")
+		except TimeoutError as K:F.warning(f"{J}: Timeout: {K}");M+=1
+		except W as K:F.error(f"{J}: Error during i/o: {K}");H+=1
 	if C is not A:
 		C.close()
-		if I(C)in F:del F[I(C)]
+		if I(C)in G:del G[I(C)]
 P,K=A,A
 async def f():
-	A=[L.create_task(e())for A in range(B.parallel)];global P,K;P=G.datetime.now()
+	A=[L.create_task(e(A))for A in range(B.parallel)];global P,K;P=E.datetime.now()
 	for C in A:await C
-	K=G.datetime.now()
+	K=E.datetime.now()
 try:L.run(f())
-except KeyboardInterrupt:K=G.datetime.now();pass
+except KeyboardInterrupt:K=E.datetime.now();pass
 finally:
-	for g in F.values():g.close()
+	for g in G.values():g.close()
 	J(f"Stats:");U=R(D)+H+M;V=(K-P).total_seconds();J(f"Ok queries = {R(D)}, errors = {H}, timeouts = {M}. Total = {U}");J(f"rps = {U/V}; total time = {V}")
 	if R(D)>0:J(f"Avg = {T.mean(D)}, min = {min(D)}, max = {max(D)}, 99% = {T.quantiles(D,n=100,method='inclusive')[98]}")
 	else:J('len(TIMES) == 0, no statistics')
